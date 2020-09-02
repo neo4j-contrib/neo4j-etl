f72b5e16a17eb7cec213b4a65e55ae88ed12b906
Adam Cowley <github@adamcowley.co.uk>
2020-09-02 09:23:46 +0100

    Fixed images in table, modified local preview

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const express = require('express')
 const app = express()
 app.use(express.static('./build/site'))
 
-app.get('/', (req, res) => res.redirect('/neo4j-etl-docs/1.5.0'))
+app.get('/', (req, res) => res.redirect('/etl-tool/1.5.0'))
 
 app.listen(8000, () => console.log('📘 http://localhost:8000'))
